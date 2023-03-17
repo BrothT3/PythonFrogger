cd1a716f5791fb6f05675d1d849cc72761f84152
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2023-03-17 11:32:12 +0100

    PlayerDeathPossible

--- GameWorld.py
+++ GameWorld.py
@@ -50,7 +50,6 @@ class GameWorld(metaclass=Singleton):
     def get_gameobjects(self):
         return self._gameobjects
 
-
     def get_player(self):
         return self._player
 
@@ -61,33 +60,26 @@ class GameWorld(metaclass=Singleton):
         pygame.font.init()
         screen = pygame.display.set_mode((1200, 700))
 
-
         pygame.display.set_caption("My Pygame window")
         fps = 60.0
         fpsClock = pygame.time.Clock()
         self.deltatime = 1/fps
 
-        log = Log("Sprites\LogSprites\Log1.png",300, 600, False, 0)
+        log = Log("Sprites\LogSprites\Log1.png", 300, 600, False, 0)
         log.shouldmove = False
         self._gameobjects.append(log)
-        
-        #self._gameobjects.append(log)
+
+        # self._gameobjects.append(log)
         player = Player("Sprites/Player/Player1.png")
         player.rect.x = 300
         player.rect.y = 580
         self._player.append(player)
-        
+
         # gameloop
         while True:
             self.update(self, self.deltatime)
             self.draw(self, screen)
             self.deltatime = fpsClock.tick(fps) / 1000.0
-            
-        
-  
-
-
-            
 
     def update(self, dt):
         for event in pygame.event.get():
@@ -108,7 +100,7 @@ class GameWorld(metaclass=Singleton):
 
         for p in self.get_player(self):
             p.draw(screen)
-        
+
         self.score.draw(screen)
 
         pygame.display.update()
@@ -129,9 +121,8 @@ class GameWorld(metaclass=Singleton):
         self.leveltime(self)
         self.collisionCheck(self)
         self.score.countup()
-        
+
         self.openmenu(self)
-        
 
     def openmenu(self):
         keys = pygame.key.get_pressed()
@@ -171,25 +162,32 @@ class GameWorld(metaclass=Singleton):
     def changelevel(self):
         if self.newlevel > self.currentlevel:
             LogSpawnerMan.disableSpawn(self.logSpawnerMan, self.newlevel)
-            #LogSpawnerMan.setlogspeed(self.logSpawnerMan, self.newlevel)
+            # LogSpawnerMan.setlogspeed(self.logSpawnerMan, self.newlevel)
             self.currentlevel = self.newlevel
             print(f"you are on level {self.currentlevel}")
 
     def collisionCheck(self):
         for p in self._player:
             for go in self._gameobjects:
-                #abs to make sure the distance is right regardless of if it's positive or negative
-                if  (go.tag == "Log" and 
+                # abs to make sure the distance is right regardless of if it's positive or negative
+                if (go.tag == "Log" and
                     abs(go.sprite.rect.bottom - p.rect.bottom) < 50 and
-                    go.shouldmove):
+                        go.shouldmove):
                     if p.rect.colliderect(go.sprite.rect):
                         go.onCollision(p)
+        self.playertouchinglog(self)
 
-                
-                    
-                
 
-                        
+    def playertouchinglog(self):
+        #check if player is colliding with any log                
+        p = self._player[0]
+        try:
+            collides = [x for x in self._gameobjects if (abs(
+                x.sprite.rect.bottom - p.rect.bottom) < 50 and p.rect.colliderect(x.sprite.rect))]
+            if isinstance(collides[0], Log):
+                p.isdead = False
+        except IndexError:
+            p.isdead = True
 
 
 if __name__ == '__main__':
